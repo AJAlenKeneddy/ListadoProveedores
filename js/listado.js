778ab81f5483d68e0f11fa22ffc867960a180159
Alen Keneddy Aguilar Jara <alenaguilar24@gmail.com>
2024-07-22 21:15:15 -0500

    Subida de Archivos

---
+++ js/listado.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    fetch('http://gmingenieros.somee.com/api/Consultas/Obtener_Todos_Los_Proveedores', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const tablaBody = document.getElementById('tabla-body');
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${item.ruc}</td>
+                <td>${item.nombre}</td>
+                <td>${item.ubigeo}</td>
+                <td>${item.departamento}</td>
+                <td>${item.provincia}</td>
+                <td>${item.distrito}</td>
+                <td>${item.direccion}</td>
+                
+            `;
+            tablaBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error al obtener los datos:', error));
+});
